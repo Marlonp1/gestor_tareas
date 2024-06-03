9cd68beb6b303a7617e8ac81d108c351b46f3eb0
dgamarra <dgamarra@continental.edu.pe>
2024-06-03 14:51:57 -0500

    Listo lanzamiento 1.0.0

--- src/logica/gestor_tareas.py
+++ src/logica/gestor_tareas.py
@@ -1,6 +1,3 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
-
 class Tarea:
     def __init__(self, titulo, descripcion):
         self.titulo = titulo
@@ -9,4 +6,11 @@ class Tarea:
 
 
 class GestorTareas:
-    pass
+    def __init__(self):
+        self.tareas = []
+
+    def agregar_tarea(self, titulo, descripcion):
+        if not titulo:
+            raise ValueError("El título no puede estar vacío")
+        tarea = Tarea(titulo, descripcion)
+        self.tareas.append(tarea)
